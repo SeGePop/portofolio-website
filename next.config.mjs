9c248fb78f1dc595afaa1b81530211908a65ed5c
Pop Sergiu <sergiugeorgepop@gmail.com>
2024-05-15 14:41:42 +0300

    First Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.module.rules.push({
+            test: /\.pdf$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]',
+                    },
+                },
+                {
+                    loader: 'pdf-loader',
+                },
+            ],
+        });
+        return config;
+    },
+};
 
 export default nextConfig;
